4c54908f94f98bf907ad4b044daa69cafa714863
Alexandr <sasharom1998@mail.ru>
2020-01-16 15:37:37 +0300

    Добавлены поля для работы с информацией конакта

--- app/src/main/java/com/example/contacts/AddChangeInformation.java
+++ app/src/main/java/com/example/contacts/AddChangeInformation.java
@@ -3,8 +3,13 @@ package com.example.contacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class AddChangeInformation extends AppCompatActivity {
+
+    private ImageButton image;
+    private EditText surname,nam,patronymic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
