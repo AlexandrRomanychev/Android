6b3654ea95de34c2de35b70a2293f918964daab1
BarasaNik <48520140+BarasaNik@users.noreply.github.com>
2020-01-17 22:41:25 +0300

    Изменены названия переменных
    Изменены названия переменных

--- app/src/main/java/com/example/contacts/AddChangeInformation.java
+++ app/src/main/java/com/example/contacts/AddChangeInformation.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -14,18 +15,19 @@ public class AddChangeInformation extends AppCompatActivity {
 
     static final int GALLERY_REQUEST = 1;
 
-    TextView Surname;
-    TextView TellPhone;
-    SimpleDraweeView draweeView;
+    private EditText surname;
+    private EditText name;
+    private EditText patronymic;
+    private EditText phone;
+    private SimpleDraweeView ImageProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_change_information);
 
-
-        draweeView = (SimpleDraweeView) findViewById(R.id.image_human);
-        draweeView.setOnClickListener(new View.OnClickListener() {
+        ImageProfile = (SimpleDraweeView) findViewById(R.id.image_human);
+        ImageProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Переход к галпереи фотографий
@@ -44,11 +46,11 @@ public class AddChangeInformation extends AppCompatActivity {
             String Name = arguments.getString("Name");
             String PhoneNumber = arguments.getString("Tellephone");
 
-            Surname = (TextView) findViewById(R.id.surname);
-            TellPhone = (TextView) findViewById(R.id.phone);
+            name = (EditText) findViewById(R.id.name);
+            phone = (EditText) findViewById(R.id.phone);
 
-            Surname.setText(Name);
-            TellPhone.setText(PhoneNumber);
+            name.setText(Name);
+            phone.setText(PhoneNumber);
         }
     }
 
@@ -60,7 +62,7 @@ public class AddChangeInformation extends AppCompatActivity {
             case GALLERY_REQUEST:
                 if(resultCode == RESULT_OK){
                     Uri selectedImage = imageReturnedIntent.getData(); // получаю uri фотографий
-                    draweeView.setImageURI(selectedImage); // установка изображения
+                    ImageProfile.setImageURI(selectedImage); // установка изображения
                 }
         }
     }
