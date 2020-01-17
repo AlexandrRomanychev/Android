8ac0a4728cc3602c22b6ccf179db04d2a7d1ea1b
BarasaNik <48520140+BarasaNik@users.noreply.github.com>
2020-01-17 05:04:36 +0300

    Добавлен код для импорта контакта из телефонной книги
    Добавлен код для импорта контакта из телефонной книги. А так же проверка на права

--- app/src/main/java/com/example/contacts/AddChangeInformation.java
+++ app/src/main/java/com/example/contacts/AddChangeInformation.java
@@ -3,12 +3,40 @@ package com.example.contacts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AddChangeInformation extends AppCompatActivity {
+
+    TextView Surname;
+    TextView TellPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_change_information);
+
+        // Получаю все аргументы
+        Bundle arguments = getIntent().getExtras();
+
+        // Получаю действие добавления контакта
+        String activity = arguments.getString("Activity");
+
+        // Если это импорт, то подставляем данные в TextView профиля
+        if (activity.equalsIgnoreCase("Import")) {
+            String id = arguments.getString("Id");
+            String Name = arguments.getString("Name");
+            String PhoneNumber = arguments.getString("Tellephone");
+
+            Surname = (TextView) findViewById(R.id.surname);
+            TellPhone = (TextView) findViewById(R.id.phone);
+
+            Surname.setText(Name);
+            TellPhone.setText(PhoneNumber);
+        }
+        else // Создание в ручную
+        {
+            Surname.setText("");
+            TellPhone.setText("");
+        }
     }
 }
--- app/src/main/java/com/example/contacts/Profile.java
+++ app/src/main/java/com/example/contacts/Profile.java
@@ -1,9 +1,16 @@
 package com.example.contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,6 +20,9 @@ public class Profile extends AppCompatActivity {
     private Button btn_import, btn_new;
     private ImageButton btn_add;
     private Boolean flag = false;
+
+    private static final int CONTACT_PICK_RESULT = 1;
+    private static final int REQUEST_CODE_PERMISSION_READ_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,20 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // переход на страницу импорта контакта
+                int permissionStatus = ContextCompat.checkSelfPermission(Profile.this, Manifest.permission.READ_CONTACTS);
+
+                //Проверка на разрешение чтения контактов
+                if (permissionStatus == PackageManager.PERMISSION_GRANTED)
+                {
+                    Intent intent2 = new Intent();
+                    Intent contactPickerIntent = new Intent(intent2.ACTION_PICK,
+                            android.provider.ContactsContract.Contacts.CONTENT_URI);
+                    startActivityForResult(contactPickerIntent, CONTACT_PICK_RESULT);
+                }
+                else {
+                    ActivityCompat.requestPermissions(Profile.this, new String[] {Manifest.permission.READ_CONTACTS},
+                            REQUEST_CODE_PERMISSION_READ_CONTACTS);
+                }
             }
         });
 
@@ -56,5 +80,59 @@ public class Profile extends AppCompatActivity {
                 flag = !flag;
             }
         });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        String PhoneNumber = "";
+
+        if (resultCode == RESULT_OK)
+        {
+            switch (requestCode)
+            {
+                case CONTACT_PICK_RESULT:
+                    Uri contactData = data.getData();
+                    Cursor c = getContentResolver().query(contactData, null, null, null, null);
+                    if (c.moveToNext())
+                    {
+                        String Id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+                        String Name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                        int CountPhone = Integer.parseInt(c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+
+                        if (CountPhone > 0)
+                        {
+                            Cursor phones = getContentResolver().query(
+                                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                    null,
+                                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                    new String[]{Id},
+                                    null);
+
+                            while (phones.moveToNext())
+                            {
+                                // Берем по умолчанию первый телефон
+                                PhoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                break;
+                            }
+                            phones.close();
+                        }
+                        // создание объекта Intent для запуска add_change_information.xml
+                        Intent intent = new Intent(this, AddChangeInformation.class);
+
+                        // передача информации о выбранном контакте
+                        intent.putExtra("ID", Id);
+                        intent.putExtra("Name", Name);
+                        intent.putExtra("Tellephone", PhoneNumber);
+                        intent.putExtra("Activity", "Import");
+
+                       startActivity(intent);
+                       setContentView(R.layout.add_change_information);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 }
