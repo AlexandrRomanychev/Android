f3c08613b5cc513976d323bda1c5628a5eba8899
BarasaNik <48520140+BarasaNik@users.noreply.github.com>
2020-01-20 13:29:30 +0300

    Добавлен класс для WorkManager и запуск во Fresco
    Код взял с сайта. Хотел, чтобы это оповещение просто появлялось в течение минуты, но не выходит. Основные действия происходит в doWork() функции.

--- app/src/main/java/com/example/contacts/MyFresco.java
+++ app/src/main/java/com/example/contacts/MyFresco.java
@@ -1,8 +1,18 @@
 package com.example.contacts;
 
 import android.app.Application;
+import android.content.Context;
+
+import androidx.work.Constraints;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.WorkManager;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import androidx.work.PeriodicWorkRequest;
 
 
 public class MyFresco extends Application {
@@ -11,5 +21,16 @@ public class MyFresco extends Application {
     public void onCreate() {
         super.onCreate();
         Fresco.initialize(this);
+
+        Constraints constraints = new Constraints.Builder()
+                .setRequiresCharging(true)
+                .build();
+
+        PeriodicWorkRequest saveRequest =
+                new PeriodicWorkRequest.Builder(UploadWorker.class, 1, TimeUnit.MINUTES)
+                        .setConstraints(constraints)
+                        .build();
+
+        WorkManager.getInstance(this).enqueue(saveRequest);
     }
 }
---
+++ app/src/main/java/com/example/contacts/UploadWorker.java
@@ -0,0 +1,43 @@
+package com.example.contacts;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class UploadWorker extends Worker {
+
+    private static final int NOTIFY_ID = 101;
+    private static final String CHANNEL_ID = "Cat channel";
+
+    public UploadWorker(
+            @NonNull Context context,
+            @NonNull WorkerParameters params) {
+        super(context, params);
+    }
+
+    @Override
+    public Result doWork() {
+        // Do the work here--in this case, upload the images.
+
+        Context context = getApplicationContext();
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.img_button_add_profile_select_false)
+                        .setContentTitle("Напоминание")
+                        .setContentText("Пора покормить кота")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(context);
+        notificationManager.notify(NOTIFY_ID, builder.build());
+
+
+
+        // Indicate whether the task finished successfully with the Result
+        return Result.success();
+    }
+}
