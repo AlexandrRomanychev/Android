e7f6576e11301f2479d295a13187cb6b57ed3d1c
Alexandr <sasharom1998@mail.ru>
2020-01-21 02:18:15 +0300

    Изменен дизайн

--- app/src/main/java/com/example/contacts/ContactInfo.java
+++ app/src/main/java/com/example/contacts/ContactInfo.java
@@ -1,16 +1,12 @@
 package com.example.contacts;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Build;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
@@ -31,6 +27,12 @@ public class ContactInfo {
 
     private Profile context;
     private Contact contact;
+    private final float TEXT_SIZE = 24f;
+    private final float BUTTON_TEXT_SIZE = 8f;
+    private final int IMAGE_WIDTH = 200;
+    private final int IMAGE_HEIGHT = 300;
+    private final int TEXT_MARGINE = 20;
+    private final int VIEW_MARGINE = 50;
 
     private static final int REQUEST_CODE_PERMISSION_CALL_PHONE= 1;
 
@@ -42,10 +44,11 @@ public class ContactInfo {
     private TextView generateLocalTextView(String text){
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(0,50,0,0);
+        layoutParams.setMargins(0,TEXT_MARGINE,0,TEXT_MARGINE);
         TextView textView = new TextView(this.context);
         textView.setText(text);
-        textView.setTextSize(24);
+        textView.setTextSize(TEXT_SIZE);
+        textView.setBackgroundResource(R.color.textField);
         textView.setLayoutParams(layoutParams);
         return textView;
     }
@@ -53,10 +56,10 @@ public class ContactInfo {
     private SimpleDraweeView generateLocalImage(){
         Uri imageUri = Uri.parse(this.contact.photo);
         SimpleDraweeView imageProfile = new SimpleDraweeView(context);
-        imageProfile.setMinimumWidth(200);
-        imageProfile.setMinimumHeight(300);
-        imageProfile.setMaxWidth(200);
-        imageProfile.setMaxHeight(300);
+        imageProfile.setMinimumWidth(IMAGE_WIDTH);
+        imageProfile.setMinimumHeight(IMAGE_HEIGHT);
+        imageProfile.setMaxWidth(IMAGE_WIDTH);
+        imageProfile.setMaxHeight(IMAGE_HEIGHT);
         if (this.contact.photo.equals(""))
             imageProfile.setImageResource(R.mipmap.ic_launcher_round);
         else
@@ -87,7 +90,8 @@ public class ContactInfo {
                 LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         Button delete = new Button(this.context);
         delete.setText(text);
-        delete.setTextSize(8);
+        delete.setTextSize(BUTTON_TEXT_SIZE);
+        delete.setBackgroundResource(R.color.delete);
         delete.setLayoutParams(layoutParams);
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,8 +108,9 @@ public class ContactInfo {
                 LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         Button congratulate = new Button(this.context);
         congratulate.setText(text);
-        congratulate.setTextSize(8);
+        congratulate.setTextSize(BUTTON_TEXT_SIZE);
         congratulate.setLayoutParams(layoutParams);
+        congratulate.setBackgroundResource(R.color.congratulation);
         congratulate.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
@@ -135,9 +140,25 @@ public class ContactInfo {
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         layoutParams.weight = 0.4f;
+        layoutParams.setMargins(VIEW_MARGINE,0,0,0);
         HorizontalScrollView horizontalScrollView = new HorizontalScrollView(context);
         horizontalScrollView.setLayoutParams(layoutParams);
         horizontalScrollView.addView(nameAndDate);
+
+        nameAndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, AddChangeInformation.class);
+                intent.putExtra("Name", contact.name);
+                intent.putExtra("Tellephone", contact.phone);
+                intent.putExtra("date", new DateConverter().dateToString(contact.date));
+                intent.putExtra("photo", contact.photo);
+                intent.putExtra("status", "update");
+                intent.putExtra("id", contact.uid);
+                intent.putExtra("login", contact.login);
+                context.startActivity(intent);
+            }
+        });
 
         return horizontalScrollView;
     }
@@ -145,6 +166,7 @@ public class ContactInfo {
     private View generateLocalButtons(){
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        layoutParams.setMargins(VIEW_MARGINE, 0, 0, 0);
         layoutParams.weight = 0.5f;
         LinearLayout buttons = new LinearLayout(context);
         buttons.setOrientation(LinearLayout.VERTICAL);
@@ -160,29 +182,14 @@ public class ContactInfo {
         // Компонент, который содержит фото и предыдущий компонент
         LinearLayout full = new LinearLayout(context);
         RelativeLayout.LayoutParams fullLayoutParams = new RelativeLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, 300);
-        fullLayoutParams.setMargins(50,0,50, 50);
+                LinearLayout.LayoutParams.MATCH_PARENT, IMAGE_HEIGHT);
+        fullLayoutParams.setMargins(VIEW_MARGINE,0,VIEW_MARGINE, VIEW_MARGINE);
         full.setOrientation(LinearLayout.HORIZONTAL);
         full.setLayoutParams(fullLayoutParams);
 
         full.addView(generateLocalImage());
         full.addView(generateLocalTextFields());
         full.addView(generateLocalButtons());
-
-        full.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, AddChangeInformation.class);
-                intent.putExtra("Name", contact.name);
-                intent.putExtra("Tellephone", contact.phone);
-                intent.putExtra("date", new DateConverter().dateToString(contact.date));
-                intent.putExtra("photo", contact.photo);
-                intent.putExtra("status", "update");
-                intent.putExtra("id", contact.uid);
-                intent.putExtra("login", contact.login);
-                context.startActivity(intent);
-            }
-        });
 
         return full;
     }
